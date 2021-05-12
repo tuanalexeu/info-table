c6e8d283f56501d131b1a3fdc8fbec7a4b768f92
tuanalexeu <alekseytyan45@gmail.com>
2021-05-12 20:10:23 +0300

    Change jms config, change JMS Listener (MDB)

--- src/main/java/com/alekseytyan/infotable/bean/MessageDrivenBean.java
+++ src/main/java/com/alekseytyan/infotable/bean/MessageDrivenBean.java
@@ -1,6 +1,8 @@
 package com.alekseytyan.infotable.bean;
 
 import lombok.SneakyThrows;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
@@ -8,21 +10,20 @@ import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
 
-@MessageDriven(activationConfig = {
-        @ActivationConfigProperty(
-                propertyName = "acknowledgeMode",
-                propertyValue = "Auto-acknowledge"
-        ),
-        @ActivationConfigProperty(
-                propertyName = "destination",
-                propertyValue = "infoTable"
-        ),
+@MessageDriven(name = "Listener", activationConfig = {
         @ActivationConfigProperty(
                 propertyName = "destinationType",
-                propertyValue = "javax.jms.Queue"
-        ),
+                propertyValue = "javax.jms.Topic"),
+        @ActivationConfigProperty(
+                propertyName = "destinationLookup",
+                propertyValue = "java:/jms/topic/Logiweb"),
+        @ActivationConfigProperty(
+                propertyName = "acknowledgeMode",
+                propertyValue = "Auto-acknowledge")
 })
 public class MessageDrivenBean implements MessageListener {
+
+    private final static Logger logger = LoggerFactory.getLogger(MessageDrivenBean.class);
 
     private final WebSocket webSocket;
 
@@ -33,6 +34,9 @@ public class MessageDrivenBean implements MessageListener {
     @SneakyThrows
     @Override
     public void onMessage(Message message) {
+
+        logger.info("Message received: " + message);
+
         TextMessage textMessage = (TextMessage) message;
         webSocket.interpretMsg(textMessage.getText());
     }
