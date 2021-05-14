67a0da5369bb0e085ed5600c836a001cdedb248c
tuanalexeu <alekseytyan45@gmail.com>
2021-05-14 17:09:14 +0300

    Add ActiveMQ support

--- src/main/java/com/alekseytyan/infotable/bean/MessageDrivenBean.java
+++ src/main/java/com/alekseytyan/infotable/bean/MessageDrivenBean.java
@@ -10,13 +10,13 @@ import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
 
-@MessageDriven(name = "Listener", activationConfig = {
+@MessageDriven(name = "DemoMDB", activationConfig = {
+        @ActivationConfigProperty(
+                propertyName = "destinationLookup",
+                propertyValue = "java:global/remoteContext/Logiweb"),
         @ActivationConfigProperty(
                 propertyName = "destinationType",
                 propertyValue = "javax.jms.Topic"),
-        @ActivationConfigProperty(
-                propertyName = "destinationLookup",
-                propertyValue = "java:/jms/topic/Logiweb"),
         @ActivationConfigProperty(
                 propertyName = "acknowledgeMode",
                 propertyValue = "Auto-acknowledge")
