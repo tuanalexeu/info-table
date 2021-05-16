b277c37a74c32990ba09f488f73cf80a8f406bb7
tuanalexeu <alekseytyan45@gmail.com>
2021-05-16 23:38:16 +0300

    Delete unused files

--- src/main/java/com/alekseytyan/infotable/binder/EventOne.java
+++
@@ -1,9 +0,0 @@
-//package com.alekseytyan.infotable.binder;
-//
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//@Getter @Setter
-//public class EventOne {
-//    private boolean enabled;
-//}
